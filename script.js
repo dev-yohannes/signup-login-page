722e154fcd11873a3d659a73f831748778cd3a53
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-16 22:28:13 +0300

    adding fieldset on the html's and an event listener for login clear

--- script.js
+++ script.js
@@ -16,16 +16,30 @@ const newsFeed = [
   },
 ];
 
+// functions
+function checkingUser() {
+  if (
+    loginUsernameInput.value === localStorage.getItem("username") &&
+    loginPasswordInput.value === localStorage.getItem("password")
+  ) {
+    console.log(newsFeed);
+  } else {
+    alert("Wrong username or password. Try again");
+  }
+}
+
+console.log(signupSignupButton);
+
+// signup section
 signupSignupButton.addEventListener("click", function (e) {
   e.preventDefault();
-
-  localStorage.setItem("username", signupUsernameInput.value);
-  localStorage.setItem("password", signupPasswordInput.value);
 
   if (signupUsernameInput.value === "" || signupPasswordInput.value === "") {
     alert("You need to fill the inputs.");
   } else {
     window.location.href = "login.html";
+    localStorage.setItem("username", signupUsernameInput.value);
+    localStorage.setItem("password", signupPasswordInput.value);
   }
 });
 
@@ -34,4 +48,20 @@ signupClearButton.addEventListener("click", function (e) {
 
   signupUsernameInput.value = "";
   signupPasswordInput.value = "";
+});
+
+// login section
+loginLoginButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  alert("hello");
+
+  // checkingUser();
+});
+
+loginClearButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  loginUsernameInput.value = "";
+  loginPasswordInput.value = "";
 });
