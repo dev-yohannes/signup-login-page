1d843a0fc412228402cc843d8f49137dceae0a80
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-18 20:55:17 +0300

    creating password strength checker, it worked this time

--- signup.js
+++ signup.js
@@ -3,8 +3,28 @@ const signupPasswordInput = document.querySelector("#signup-password-input");
 let signupSignupButton = document.querySelector("#signup-signup-button");
 let signupClearButton = document.querySelector("#signup-clear-button");
 let passwordStrength = document.querySelector("#password-strength");
+let viewIcon = document.querySelector("#view-icon");
 
 // function
+
+function passwordStrengthChecker() {
+  if (signupPasswordInput.value.length <= 3) {
+    passwordStrength.innerHTML = "Weak";
+    passwordStrength.style.color = "red";
+    // signupSignupButton.disabled = true;
+  } else if (signupPasswordInput.value.length <= 5) {
+    passwordStrength.innerHTML = "Medium";
+    passwordStrength.style.color = "yellow";
+    // signupSignupButton.disabled = true;
+  } else if (signupPasswordInput.value.length >= 7) {
+    passwordStrength.innerHTML = "Strong";
+    passwordStrength.style.color = "green";
+    signupConditional();
+
+    // signupSignupButton.disabled = false;
+  }
+}
+
 function signupConditional() {
   if (signupUsernameInput.value === "" || signupPasswordInput.value === "") {
     alert("You need to fill the inputs.");
@@ -15,34 +35,15 @@ function signupConditional() {
   }
 }
 
-function passwordStrengthChecker() {
-  if (signupPasswordInput.length > 3) {
-    passwordStrength.innerHTML = "Weak";
-    console.log("weak");
-  } else if (signupPasswordInput.length > 5) {
-    passwordStrength.innerHTML = "Medium";
-    console.log("medium");
-  } else if (signupPasswordInput.length > 7) {
-    passwordStrength.innerHTML = "Strong";
-    console.log("strong");
-  }
-}
-
-signupPasswordInput.addEventListener("mouseleave", function () {
-  // passwordStrength.innerHTML = console.log(passwordStrengthChecker());
-
-  passwordStrengthChecker();
-});
-
 signupSignupButton.addEventListener("click", function (e) {
   e.preventDefault();
-  signupConditional();
+  passwordStrengthChecker();
 });
 
 document.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     e.preventDefault();
-    signupConditional();
+    passwordStrengthChecker();
   }
 });
 
@@ -51,4 +52,10 @@ signupClearButton.addEventListener("click", function (e) {
 
   signupUsernameInput.value = "";
   signupPasswordInput.value = "";
+});
+
+viewIcon.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  signupPasswordInput.type = "text";
 });
